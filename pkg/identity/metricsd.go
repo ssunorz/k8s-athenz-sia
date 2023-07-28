b00b162abbcfe01ac37892988c4241fd1c74ed93
Jeongwoo Kim - jekim <jekim@yahoo-corp.jp>
2023-07-28 14:13:47 +0900

    fix: no comment

--- pkg/identity/metricsd.go
+++ pkg/identity/metricsd.go
@@ -63,8 +63,7 @@ func Metricsd(idConfig *IdentityConfig, stopChan <-chan struct{}) (error, <-chan
 				log.Warnf("Failed to read element '%s' of given TARGET_DOMAIN_ROLES: %s, err: %s", domainrole, idConfig.TargetDomainRoles, err.Error())
 				continue
 			}
-			// if RoleCertFilenameDelimiter = "_" then,
-			// fileName = your-domain_your-role.cert.pem"
+
 			fileName := targetDomain + idConfig.RoleCertFilenameDelimiter + targetRole + ".cert.pem"
 			exporter.Files = append(exporter.Files, strings.TrimSuffix(idConfig.RoleCertDir, "/")+"/"+fileName)
 		}
